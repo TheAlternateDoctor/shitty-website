8a0394a09701be9c2f4a7fad2df89a0a0e8f7148
TheAlternateDoctor <thealternatedoc@gmail.com>
2024-11-05 17:24:57 +0100

    Functional tag buttons

--- gallery.js
+++ gallery.js
@@ -1,6 +1,18 @@
+selectedTags = []
+
 function fillPics() {
     //Results of like, a query or smth
-    fetch("gallery.php")
+    uri = "gallery.php"
+    if(selectedTags.length > 0){
+        tagList = ""
+        selectedTags.forEach(element => {
+            tagList=element+","
+        });
+        tagList.substring(0, tagList.length-1)
+        uri+="?tags="+tagList
+    }
+
+    fetch(uri)
     .then(response => response.text())
     .then(data => {
         pics = JSON.parse(data)
@@ -15,5 +27,17 @@ function fillPics() {
     
         gallery.innerHTML = html
     });
+}
 
+function addTag(tag){
+    selectedTags.push(tag)
+    document.getElementById(tag).setAttribute("onclick", "removeTag('"+tag+"')")
+    fillPics();
+}
+
+function removeTag(tag){
+    index = selectedTags.indexOf(tag);
+    selectedTags.splice(index, 1);
+    document.getElementById(tag).setAttribute("onclick", "addTag('"+tag+"')")
+    fillPics();
 }
