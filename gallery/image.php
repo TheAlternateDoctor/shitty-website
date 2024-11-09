36268a120363fbab5868ced9772ca40b55736e36
TheAlternateDoctor <thealternatedoc@gmail.com>
2024-11-09 01:41:02 +0100

    Fixed gallery being cooked again

--- gallery/image.php
+++ gallery/image.php
@@ -2,13 +2,13 @@
     $id = $_GET["id"];
     include_once("db.php");
     $image = $mysqli->query("SELECT * FROM img WHERE id=" . $id)->fetch_row();
-    if(is_null($image)){
+    if(!is_null($image)){
         $tagsUnordered = $mysqli->query("SELECT tags.name FROM img_tag_link INNER JOIN tags ON tags.id = img_tag_link.tag_id WHERE img_tag_link.img_id = " . $id)->fetch_all();
         $tags = array();
         foreach ($tagsUnordered as $tag) {
             array_push($tags, $tag[0]);
         }
-        include_once("image_actual.html");
+        include_once("image_actual.php");
     } else{
         include_once("../404.html");
     }
