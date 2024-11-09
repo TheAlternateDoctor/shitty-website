99ed0b30da1d92ed86fd263c6b2334aeffe2854a
TheAlternateDoctor <thealternatedoc@gmail.com>
2024-11-09 01:37:00 +0100

    Forgot to bring over everything from the gallery, woops

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbHost="localhost";
-$dbUser="website";
-$dbPass="website";
-$dbName="gallery";
-
-$mysqli = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
--- gallery/image_actual.php
+++ gallery/image_actual.php
@@ -1,33 +1,23 @@
-<?php
-    $id = $_GET["id"];
-    include_once("db.php");
-    $image = $mysqli->query("SELECT * FROM img WHERE id=" . $id)->fetch_row();
-    $tagsUnordered = $mysqli->query("SELECT tags.name FROM img_tag_link INNER JOIN tags ON tags.id = img_tag_link.tag_id WHERE img_tag_link.img_id = " . $id)->fetch_all();
-    $tags = array();
-    foreach ($tagsUnordered as $tag) {
-        array_push($tags, $tag[0]);
-    }
-?>
 
 <HTML>
 
 <head>
     <title>Doc's website</title>
-    <link href="style.css" rel="stylesheet">
+    <link href="/style.css" rel="stylesheet">
     <script>
-        fetch("includes/sidebar.html")
+        fetch("/includes/sidebar.html")
             .then(response => response.text())
             .then(data => {
                 document.getElementById("sidebar").innerHTML = data;
             });
 
-        fetch("includes/top.html")
+        fetch("/includes/top.html")
             .then(response => response.text())
             .then(data => {
                 document.getElementById("top").innerHTML = data;
             });
 
-        fetch("includes/footer.html")
+        fetch("/includes/footer.html")
             .then(response => response.text())
             .then(data => {
                 document.getElementById("footer").innerHTML = data;
